2edecb2462d2f51115dca836072f829e3ded7756
gabrielaelisa <gabriela.mendoza@ug.uchile.cl>
2018-10-13 20:00:29 -0300

    passing test1 , deadlock somewhere :(

--- transbordo.c
+++ transbordo.c
@@ -13,6 +13,7 @@ FifoQueue q_chacao;
 nMonitor m ;//= nMakeMonitor();
 nCondition no_empty_pargua;//  = nMakeCondition(m);
 nCondition no_empty_chacao;// = nMakeCondition(m);
+nCondition no_empty;
 
 void inicializar(int p){
     q_pargua=MakeFifoQueue();
@@ -20,6 +21,7 @@ void inicializar(int p){
     m= nMakeMonitor();
     no_empty_chacao= nMakeCondition(m);
     no_empty_pargua= nMakeCondition(m);
+    no_empty= nMakeCondition(m);
     for(int i=0; i<p ; i++){
         Transbordador * t = nMalloc(sizeof(Transbordador));
         t->id= i;
@@ -28,48 +30,57 @@ void inicializar(int p){
 }
 void transbordoAChacao(int v){
     nEnter(m);
+    nPrintf("norteno toma barco\n");
     Transbordador * my_t;
+
+    while(EmptyFifoQueue(q_chacao)&& EmptyFifoQueue(q_pargua))
+        nWaitCondition(no_empty);
+
     if (EmptyFifoQueue(q_pargua)){
         my_t = (Transbordador *) GetObj(q_chacao);
-        nNotifyAll(m);
         nExit(m);
         haciaPargua(my_t->id, -1);
         haciaChacao(my_t->id, v);
+        nPrintf("norteno llega a destino\n");
     }
     else{
         my_t= (Transbordador *) GetObj(q_pargua);
-        nNotifyAll(m);
         nExit(m);
         haciaChacao(my_t->id, v);
+        nPrintf("norteno llega a destino\n");
     }
     nEnter(m);
     PushObj(q_chacao,my_t);
-    nNotifyAll(m);
+    nPrintf("nuevo barco en chacao\n");
+    nSignalCondition(no_empty);
     nExit(m);
 
 }
 void transbordoAPargua(int v){
     nEnter(m);
-    nPrintf("primer transbordo\n");
+    nPrintf("isleno toma barco\n");
     Transbordador * my_t;
+    while(EmptyFifoQueue(q_chacao)&& EmptyFifoQueue(q_pargua)){
+        nPrintf("esperando\n");
+        nWaitCondition(no_empty);}
+
     if (EmptyFifoQueue(q_chacao)){
-        nPrintf("fifo vacía\n");
         my_t = (Transbordador *) GetObj(q_pargua);
-        nPrintf("my t\n");
-        nNotifyAll(m);
         nExit(m);
+        nPrintf("exit mutex\n");
         haciaChacao(my_t->id, -1);
+        nPrintf("isleno llega a destino\n");
         haciaPargua(my_t->id, v);
     }
     else{
         my_t= (Transbordador *) GetObj(q_chacao);
-        nNotifyAll(m);
         nExit(m);
+        nPrintf("isleno llega a destino\n");
         haciaPargua(my_t->id, v);
     }
     nEnter(m);
     PushObj(q_pargua,my_t);
-    nNotifyAll(m);
+    nSignalCondition(no_empty);
     nExit(m);
 
 
